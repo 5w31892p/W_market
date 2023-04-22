6a196e217debfb16c52c16972ac30aaa691c0a90
Seowoo <5w31892p@gmail.com>
2023-04-22 14:06:29 +0900

    <feat> Default Settings

---
+++ src/test/java/me/woo/wmarket/WmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package me.woo.wmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmarketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
