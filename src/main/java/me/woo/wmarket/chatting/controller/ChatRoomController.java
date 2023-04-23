bc6c78161b1a0d8d74a4a89156500e029af5a842
Seowoo <5w31892p@gmail.com>
2023-04-23 18:59:30 +0900

    <feat> chatting settings

---
+++ src/main/java/me/woo/wmarket/chatting/controller/ChatRoomController.java
@@ -0,0 +1,5 @@
+package me.woo.wmarket.chatting.controller;
+
+public class ChatRoomController {
+
+}
---
+++ src/main/java/me/woo/wmarket/chatting/service/ChatMessageService.java
@@ -0,0 +1,5 @@
+package me.woo.wmarket.chatting.service;
+
+public interface ChatMessageService {
+
+}
